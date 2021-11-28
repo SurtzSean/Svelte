3e2cc6aaa6c65f9e8ade2d36d5bfcc572a265642
Sean Surtz <surtzsean@gmail.com>
2021-11-28 00:30:41 -0500

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Header from "./components/Header.svelte";
+	import List from "./components/List.svelte";
+</script>
+
+<style>
+	.container {
+		width: 500px;
+		background: teal;;
+	}
+</style>
+
+<div class="container">
+	<Header></Header>
+	<List></List>
+</div>
---
+++ src/components/Header.svelte
@@ -0,0 +1,24 @@
+<script>
+    import {items} from '../stores.js';
+    $: itemCount = $items.length;
+    $: completedItemCount = $items.filter(item => item.completed).length;
+</script>
+
+<style>
+ .header {
+     display: flex;
+     justify-content: space-between;
+     align-items: center;
+     padding: 15px 20px;
+     color: aliceblue;
+     font-weight: bold;
+     font-size: 1.5em;
+     background: rgba(0, 0, 0, .1);
+
+ }
+</style>
+
+<div class="header">
+    <span>Sean's todo list</span>
+    <span>{completedItemCount} / {itemCount} completed</span>
+</div>
